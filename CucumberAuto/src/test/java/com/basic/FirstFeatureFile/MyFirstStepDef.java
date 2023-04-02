409a5ebb3486021a43e9ab096e5cb5fd169befe3
milt_toni <nihora@hotmail.com>
2023-04-02 03:44:46 +0200

    Second commit

--- CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -18,6 +18,7 @@ public void user_need_to_be_on_facebook_login_page() {
 	System.setProperty("webdriver.chrome.driver", "D:\\CUCUMBER_SETUP\\chromedriver_win32\\chromedriver.exe");
 	driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
+		System.out.println("opening Url");
   
 }
 
