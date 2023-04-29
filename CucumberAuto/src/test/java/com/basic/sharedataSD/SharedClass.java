c0ca89635e7224be8401ad8d8808438f01171084
milt_toni <nihora@hotmail.com>
2023-04-29 22:22:19 +0200

    20th commith

--- CucumberAuto/src/test/java/com/basic/sharedataSD/SharedClass.java
+++ CucumberAuto/src/test/java/com/basic/sharedataSD/SharedClass.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SharedClass {
 	
@@ -16,6 +17,7 @@ public class SharedClass {
 	public WebDriver setup(){
 		if(driver == null){
 			System.setProperty("webdriver.chrome.driver", "D:\\CUCUMBER_SETUP\\chromedriver_win32\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();	
 						
 		}
